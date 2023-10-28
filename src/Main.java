a5eec07192c0bf902c184892592095d0c98fa499
Andrew <andy@andeh.uk>
2023-10-28 18:11:13 +0100

    Reading Java

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,31 @@
 public class Main {
-    // Data Types
+    // Reading Java Code / Syntax
 
     public static void main(String[] args) {
-        // Numerical Types
+        /* How to read Java code / syntax */
 
-        // Commonly Used
-        int aNumber = 42; // Integer (Whole Number) [-2,147,483,648 to 2,147,483,647]
-        float aFloatingNumber = 13.37f; // Floating point number (7 decimal point precision)
-        double aDoubleNumber = 420.0005f; // Double number (15 decimal point precision)
-        // 1.412 * 10^4 = 14120
+        // This is a single line comment
 
-        // Sometimes Used
-        byte aTinyNumber = 100; // One byte (or 8 bits) stores numbers from -128 to 127
-        short aSmallNumber = 30000; // shorts store numbers from -32,768 to 32,767
-        long aLongNumber = 50000000; // longs store numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        /*
+         *
+         * This is a
+         * Multiline Comment
+         *
+         * */
 
-        // Booleans (Logic)
+        // Declaring a variable | ALl lines end with a semicolon (;)
+        int i;
 
-        boolean aTruthyValue = true;
-        boolean aFalsyValue = false;
+        // Assigning a Variable
+        i = 30;
 
-        // Characters and Strings
+        // Declaring and Assigning (Initialising)
+        int x = 10;
 
-        char aSingleCharacter = 'A'; // Stores a single character. Can also include special characters like punctuation
-        // Must use single quotes, not double, for a char value
-
-        String message = "Hello World!";
-        String test = null; // null is not 0, but is literally empty. It has no value. Also, a falsy value.
+        // Access variables / methods of Objects
+        // Other data types are coloured because they are primitive data types
+        // A String type is not primitive, it is essentially an array of char values
+        String s = "Hello World!";
+        s.length();
     }
 }
