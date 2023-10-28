bb1bd22a7468545bf4918666cec37ca11844286b
Andrew <andy@andeh.uk>
2023-10-28 18:23:37 +0100

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
-public class Ternary {
+public class Arrays {
     public static void main(String[] args) {
-        // Ternary Operator
+        String[] questions = new String[3]; // Created a new array of strings with a maximum length of 3
+        questions[0] = "How many States does the USA have?";
+        questions[1] = "What is the capital of the United Kingdom?";
+        questions[2] = "What is the chemical symbol for Iron?";
 
-        int exam = 65;
-        int gift = 0;
+        String[] answers = new String[3];
+        answers[0] = "50";
+        answers[1] = "London";
+        answers[2] = "Fe";
 
-        // If statement version:
-        if (exam >= 50) {
-            gift = 10;
-        } else {
-            gift = 0;
-        }
-        System.out.println(gift);
+        System.out.println(questions[0]);
+        System.out.println(answers[0]);
 
-        gift = (exam >= 50) ? 10 : 0;
-        System.out.println(gift);
+        // Override value at index:
+        questions[0] = "This is an example question!";
+        answers[0] = "This is an example answer!";
+        System.out.println(questions[0]);
+        System.out.println(answers[0]);
+
+        // ArrayIndexOutOfBoundsException (Uncomment Line Below)
+        // System.out.println(questions[3]);
     }
-
 }
