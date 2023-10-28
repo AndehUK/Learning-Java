b26bae36990174d57eb61b519bcd7c5d9c7b2cce
Andrew <andy@andeh.uk>
2023-10-28 18:25:07 +0100

    Trivia Game 2

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,104 @@
-public class Methods {
-    public static void main(String[] args) {
-        // Methods
+import java.util.Scanner;
 
-        String[] questions = new String[3];
+public class TriviaGame2 {
+    // Trivia Game Exercise
+
+    // Re-create the Trivia Game from before.
+
+    /*
+     *
+     * Define (at least) five questions with five answers.
+     * use arrays to save the Qs and As
+     * Create at least 2 methods that make the code more readable and easier to follow
+     * BONUS: use an endless while loop with if statements so the user can play until they no longer want to.
+     *
+     */
+
+    public static void main(String[] args) {
+        String[] questions = new String[5];
+        String[] answers = new String[5];
+
         questions[0] = "How many States does the USA have?";
         questions[1] = "What is the Capital of the United Kingdom?";
         questions[2] = "What is the chemical symbol for Iron?";
+        questions[3] = "How high is Mount Everest? (Answer in km)";
+        questions[4] = "What is the Answer to Life, the Universe and Everything?";
 
-        String[] answers = new String[3];
         answers[0] = "50";
         answers[1] = "London";
         answers[2] = "Fe";
+        answers[3] = "8848";
+        answers[4] = "42";
 
-        outputQuestionsAndAnswersXTimes(1, questions, answers);
-        System.out.println(sum(10, 12));
-    }
+        Scanner scanner = new Scanner(System.in);
+        int score = 0;
 
-    public static void outputQuestionsAndAnswersXTimes(int loopTotal, String[] questions, String[] answers) {
-        for (int y = 0; y < loopTotal; y++) {
-            outputQuestionsAndAnswers(questions, answers);
+        while (true) {
+            displayTitle();
+
+            for (int i = 0; i < questions.length; i++) {
+                displayQuestion(i + 1, questions[i]);
+                String userAnswer = scanner.next();
+
+                if (isCorrectAnswer(userAnswer, answers[i])) {
+                    displaySuccess(userAnswer);
+                    score = addToScore(score, 1);
+                    displayScore(score);
+                } else {
+                    displayFailed(userAnswer, answers[i]);
+                }
+            }
+
+            displayEndScore(score);
+            displayPlayAgain();
+            if (scanner.next().equalsIgnoreCase("y")) {
+                score = 0;
+            } else {
+                break;
+            }
         }
+
+        goodbye();
     }
 
-    public static void outputQuestionsAndAnswers(String[] questions, String[] answers) {
-        for (int i = 0; i < questions.length; i++) {
-            System.out.println(questions[i]);
-            System.out.println(answers[i]);
-        }
+    public static void displayTitle() {
+        System.out.println("TRIVIA GAME");
+        System.out.println("-----------");
     }
 
-    public static int sum(int x, int y) {
-        return x + y;
+    public static int addToScore(int score, int points) {
+        return score + points;
+    }
+
+    public static void displayScore(int score) {
+        System.out.println("You currently have " + score + " points!");
+    }
+
+    public static void displayPlayAgain() {
+        System.out.println("Would you like to play again? (y/n)");
+    }
+
+    public static void displayEndScore(int score) {
+        System.out.println("You finished the game with " + score + " points!");
+    }
+
+    public static void goodbye() {
+        System.out.println("Thanks for playing the TRIVIA GAME!");
+    }
+
+    public static void displayQuestion(int questionNum, String question) {
+        System.out.println("Question " + questionNum + ": " + question);
+    }
+
+    public static boolean isCorrectAnswer(String userAnswer, String correctAnswer) {
+        return userAnswer.equalsIgnoreCase(correctAnswer);
+    }
+
+    public static void displaySuccess(String userAnswer) {
+        System.out.println("'" + userAnswer + "' was the correct answer, good job!");
+    }
+
+    public static void displayFailed(String userAnswer, String correctAnswer) {
+        System.out.println("'" + userAnswer + "' was NOT the correct answer, the correct answer was: '" + correctAnswer + "'!");
     }
 }
