d3c3d94e6d9a89d9bfccdf8eea624e222aaa5cec
Andrew <andy@andeh.uk>
2023-10-28 18:23:05 +0100

    Casting

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,15 @@
-public class StringMethods {
-    // Strings and String Methods
-
+public class Casting {
+    // Casting (Converting data types between each other)
+    
     public static void main(String[] args) {
-        String sentence = "I am learning Java!";
-        System.out.println(sentence);
-        System.out.println("Length of sentence is " + sentence.length() + " characters!"); // 19
 
-        // Upper and Lower Case
-        System.out.println("Shouting: " + sentence.toUpperCase());
-        System.out.println("Whispering: " + sentence.toLowerCase());
+        int loan = 750;
+        float interestRate = 0.0525f;
 
-        // indexOf
-        System.out.println("The word 'Java' is found at position: " + sentence.indexOf("Java"));
+        float implicitinterest = loan * interestRate;
+        float explicitInterest = (float) loan * interestRate;
 
-        // Replace
-        System.out.println(sentence.replace("Java", "C++"));
-
-        // Is the string empty?
-        String s = "";
-        boolean empty = s.isEmpty();
-        System.out.println("'" + s + "' => Is this string empty? " + empty);
-
-        // startsWith and endsWith
-        System.out.println("Does the sentence start with an 'I'? " + sentence.startsWith("I"));
-        System.out.println("Does this sentence end with a '?'? " + sentence.endsWith("?"));
-
-        // contains
-        System.out.println("Does this sentence contain the word 'Java'? " + sentence.contains("Java"));
-
-        // charAt
-        System.out.println("The character at index '10' is: '" + sentence.charAt(10) + "'");
-
-        // substring
-        s = sentence.substring(10);
-        System.out.println("The sentence substringed at index 10 is: " + sentence.substring(10));
+        int x = (int) 50.5f + (int) 20.6f; // Rounds down
+        System.out.println(x);
     }
 }
